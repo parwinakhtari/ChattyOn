edd2d5b0d394cfcb69d70a2d4960984f8505e943
Parwin Akhtari <68842515+parwinakhtari@users.noreply.github.com>
2021-08-31 14:08:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const WebSocket = require('ws');
 
 const port = 8080;
 const server = http.createServer(express);
-const wss = new WebSocket.Server({ server })
+const wsserver = new WebSocket.Server({ server })
 
-wss.on('connection', function connection(ws) {
+wsserver.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
+    wsserver.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
       }
